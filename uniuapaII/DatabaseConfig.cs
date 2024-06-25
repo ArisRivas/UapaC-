232a72e3d162d53972608eb9cd6c0549af9e68ee
Esmarlyn Diaz <Ediaz@intellisys.com.do>
2024-06-24 21:35:06 -0400

    Remove old proyect

--- uniuapaII/DatabaseConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uniuapaII
-{
-    public static class DatabaseConfig
-    {
-        // Replace with your actual connection string
-        public const string ConnectionString = "localhost;Database=UniUapa;User Id=sa;Password=your_password;";
-    }
-}
